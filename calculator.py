af55b654df46b8088eeb7b08c066d3073bbc286e
klenam <kojofiah@gmail.com>
2022-10-04 20:06:58 -0400

    Added a addition function

--- calculator.py
+++ calculator.py
@@ -3,5 +3,9 @@ from unicodedata import name
 
 class calculator:
 
-    def __init__(self) -> None:
-        self.number = name
+    def __init__(self,numberA,numberB) -> None:
+        self.numberA = numberA
+        self.numberB = numberB
+
+    def add(self) -> int:
+        return self.numberA + self.numberB
