2c313a4d4b3d0a5176cf87537b14a58fdbb7d917
collaborator <kojofiah@gmail.com>
2022-10-14 20:55:51 -0400

    Added a feature to multiply integers and decimal numbers

--- calculator.py
+++ calculator.py
@@ -2,13 +2,15 @@ from unicodedata import name
 
 
 class calculator:
-
-    def __init__(self,numberA,numberB) -> None:
+    def __init__(self, numberA, numberB) -> None:
         self.numberA = numberA
         self.numberB = numberB
 
     def add(self) -> int:
         return self.numberA + self.numberB
-    
-    def addingDecimals(self) ->float:
-        return self.numberA +self.numberB
+
+    def addingDecimals(self) -> float:
+        return self.numberA + self.numberB
+
+    def multiplication(self) -> float:
+        return self.numberA * self.numberB
